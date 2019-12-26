8fe2e692fdbb3af139d0735ac3c65f0181c458d0
lzc <lzc555lzc@163.com>
2019-12-26 11:09:08 +0800

    第一次提交

---
+++ src/main/java/com/atguigu/guliaaa/GuliaaaApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.guliaaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuliaaaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuliaaaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atguigu/guliaaa/GuliaaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.guliaaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliaaaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
