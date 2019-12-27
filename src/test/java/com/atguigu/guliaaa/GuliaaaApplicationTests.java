520a52b1300baa3ec24c19689de6af0660576ec8
lzc <lzc555lzc@163.com>
2019-12-27 14:59:06 +0800

    commit message

---
+++ src/test/java/com/atguigu/guliaaa/GuliaaaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.atguigu.guliaaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliaaaApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("bbbbbbbbbbbbbbb");
+        System.out.println("fffffffffffffffff");
+        System.out.println("ggggggg");
+    }
+
+}
